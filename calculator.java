1e0f4767febc441fe172d1b85174069e3a5285c1
Dimo Mitev <104933623+dimosoftuni@users.noreply.github.com>
2026-05-28 19:20:28 +0300

    Change add method call to include three arguments
    Updated the add method call to use three parameters.

--- calculator.java
+++ calculator.java
@@ -28,7 +28,7 @@ public class Calculator {
 
         Calculator calc = new Calculator();
 
-        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Add: " + calc.add(10, 5, 20));
         System.out.println("Subtract: " + calc.subtract(10, 5));
         System.out.println("Multiply: " + calc.multiply(10, 5));
         System.out.println("Divide: " + calc.divide(10, 5));
